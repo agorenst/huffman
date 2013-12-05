dc5805ba891265bee82b61fae36fd9a0b4cfd263
Aaron Gorenstein <agorenst@cs.wisc.edu>
2013-12-04 18:17:36 -0600

    completely rewrote and simplified. Now to do memory checks

--- huffman.cpp
+++ huffman.cpp
@@ -1,18 +1,15 @@
 #include <string>
+#include <list>
 #include <map>
 #include <memory>
 #include <queue>
-#include <sstream>
-#include <fstream>
 #include <iostream>
 
 using namespace std;
 
-
 // this associates a string with a double.
 // most importantly, it creates an ordering.
 class encoding {
-
     public:
         const string s; // symbol
         const double f; // frequency of symbol
@@ -38,121 +35,111 @@ ostream& operator<<(ostream& o, const encoding& e) {
     return o;
 }
 
-
-class htree {
-    private:
-        // a recursive tree, has an encoding and 2 children.
-        // either has both children, or none
-        class hnode {
-            public:
-                const shared_ptr<hnode> l, r;
-                const encoding e;
-
-                hnode() {}
-                hnode(shared_ptr<hnode> l, shared_ptr<hnode> r): l(l), r(r), e(l->e + r->e) {}
-                hnode(encoding e): l(), r(), e(e) {}
-                bool is_leaf() const { return l == NULL && r == NULL; }
-                
-                // this isn't printing, but rather saving our binary string mapping to the
-                // map.
-                void print_code(map<string, string>& m, const string parentcode, bool left) {
-                    string code = parentcode;
-                    if (left) { code += "0"; }
-                    else { code += "1"; }
-                    if (is_leaf()) {
-                        m[e.s] = code;
-                    }
-                    else {
-                        l->print_code(m, code, true);
-                        r->print_code(m, code, false);
-                    }
-                }
-        };
-
-    public:
-        // htree basically "kicks off" the recursive tree structure
-        // expressed by the hnodes
-        //
-        // by using shared pointers instead of regular ones,
-        // no explicit deletes are needed.
-        shared_ptr<hnode> me;
-
-        htree() {}
-        htree(shared_ptr<hnode> me): me(me) {}
-        htree(htree l, htree r): me(new hnode(l.me, r.me)) {}
-        htree(encoding e): me(new hnode(e)) {}
-        bool is_leaf() const { return me->is_leaf(); }
-
-        bool operator<(const htree& h) const {
-            return me->e < h.me->e;
-        }
-
-        map<string, string> encode() {
-            map<string, string> m;
-            me->l->print_code(m, "", true);
-            me->r->print_code(m, "", false);
-            return m;
-        }
+// this is the simple binary tree structure
+// we will be using in our algorithm.
+// NOTE: we can only construct either deg-0, or deg-2
+// trees! 
+struct htree {
+    const shared_ptr<htree> l, r;
+    const encoding e;
+    htree(shared_ptr<htree> l, shared_ptr<htree> r): l(l), r(r), e(l->e + r->e) {}
+    htree(encoding e): l(), r(), e(e) {}
+    bool is_leaf() const { return l == NULL && r == NULL; }
 };
 
-ostream& operator<<(ostream& o, htree h) {
-    if (h.me == NULL) { return o; }
-    return (o << h.me->e << endl << htree(h.me->l) << htree(h.me->r));
+// as we build the forest of more htrees,
+// we will be using a lot of shared_ptr<htree>s.
+// This makes it more convenient, and MOST IMPORTANTLY,
+// adds in the ordering operator, so the priority queue actually
+// works correctly.
+struct indirect_htree : public shared_ptr<htree> {
+    bool operator<(const indirect_htree& h) const {
+        return (*this)->e < h->e;
+    }
+    indirect_htree(htree* h): shared_ptr<htree>(h) {}
+};
+
+//THIS IS THE WHOLE ALGORITHM.
+// A lot of the "magic" happens thanks to the fact
+// that the priority queue works correctly:
+// hence (partly) the need for indirect_htree class.
+template<class iter>
+indirect_htree huffman(iter start, iter end) {
+    
+    // this work-queue holds our forest of trees.
+    std::priority_queue<indirect_htree> wq;
+
+    // we initialize the forest to n size-1 (ie, leaves)
+    // trees.
+    for (auto it = start; it != end; ++it) {
+        wq.push(indirect_htree(new htree(*it)));
+    }
+
+    // here is the greedy algorithm: we combine the two
+    // cheapest trees (be them size 1 or greater) into
+    // a new tree, and put that back on the queue.
+    // NB: the runtime is not as obvious from this structure as a for loop!
+    // However, we see that wq.size() decreases by 1 each time.
+    while (wq.size() > 1) {
+        auto min1 = wq.top(); wq.pop();
+        auto min2 = wq.top(); wq.pop();
+        wq.push(indirect_htree(new htree(min1,min2)));
+    }
+    return wq.top();
 }
 
-// this takes in a frequency mapping, and does the greedy algorithm
-// to build the final huffman tree.
-class huffman {
-    public:
-        htree code;
-        huffman(map<string, float> m) {
-            priority_queue<htree> wq; // my workqueue
-            for (auto it = m.begin(); it != m.end(); ++it) {
-                wq.push(htree(encoding(it->first, it->second)));
-            }
-
-            // this for loop instead of the while loop more
-            // explicitly says the runtime.
-            //int n = wq.size();
-            //for (int k = n+1; k < 2*n; ++k) {
-            // now we do the loop:
-            while (wq.size() > 1) {
-                auto min1 = wq.top(); wq.pop();
-                auto min2 = wq.top(); wq.pop();
-                wq.push(htree(min1,min2));
-            }
-            code = wq.top();
-        }
-};
-
-map<string, float> loadencs(istream& infile) {
-    map<string, float> contents;
-    string word, line;
-    double freq;
-    while (getline(infile, line)) {
-        istringstream ss(line);
-        ss >> word;
-        ss >> freq;
-        contents[word] = freq;
+// so given a huffman encoding (ie, the tree structure),
+// now we want to actually print out the encodings.
+// Here is the recursive way to do it.
+void coding_map(map<string,string>& m, shared_ptr<htree> h, string parent_string, bool left_child) {
+    string s = parent_string;
+    if (left_child) { s += "0"; }
+    else            { s += "1"; }
+    if (h->is_leaf()) {
+        m[h->e.s] = s;
     }
-    return contents;
+    else {
+        coding_map(m, h->l, s, true);
+        coding_map(m, h->r, s, false);
+    }
+}
+
+// this is the base-case of the recursion.
+map<string,string> start_coding_map(shared_ptr<htree> h) {
+    map<string,string> m;
+    coding_map(m, h->l, "", true);
+    coding_map(m, h->r, "", false);
+    return m;
+}
+
+// here is the infix printing of the htree itself.
+ostream& operator<<(ostream& o, shared_ptr<htree> h) {
+    if (h == NULL) { return o; }
+    o << h->e << endl << h->l << h->r;
+    return o;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        cout << "usage: ./huffman <filename>\n";
-        return 0;
+    std::string s;
+    double f;
+    std::list<encoding> encodings;
+    while(!cin.eof()) {
+        cin >> s;
+        cin >> f;
+        encodings.push_back(encoding(s,f));
+        //encodings.insert(encoding(s,f));
     }
-    ifstream file;
-    file.open(argv[1]);
+    // weird error with input reading loop, so we loop once more.
+    // easy remedy.
+    encodings.pop_back();
 
-    map<string, float> contents = loadencs(file);
-    file.close();
-
-    huffman h(contents);
-    cout << h.code << endl;
-    auto m = h.code.encode();
-    for (auto i = m.begin(); i != m.end(); ++i) {
-        cout << i->first << ": " << i->second << endl;
+//    for (auto it = encodings.begin(); it != encodings.end(); ++it) {
+//        cout << *it << endl;
+//    }
+    auto h = huffman(encodings.begin(), encodings.end());
+    cout << h << endl;
+    auto m = start_coding_map(h);
+    for (auto it = m.begin(); it != m.end(); ++it) {
+        cout << it->first << ": " << it->second << endl;
     }
 }
