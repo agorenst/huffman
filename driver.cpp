d6baf7e7a996f07359f64746dc45cbbef0e5a2aa
agorenst <agorenst@cs.wisc.edu>
2014-01-19 21:38:46 -0600

    ahhhh it kind of works when it knows how many bytes to decode.

--- driver.cpp
+++ driver.cpp
@@ -2,6 +2,8 @@
 #include <cmath>
 #include <fstream>
 #include <cstring>
+#include <array>
+#include <cassert>
 
 #include "huffman.h"
 //#include "feed_buffer.h"
@@ -11,160 +13,198 @@ using namespace std;
 // we treat each 8-bit char as a symbol
 const int numsymbs = 256;
 vector<string> encodings(numsymbs,"");
+shared_ptr<htree> hufftree;
 vector<double> freqs(numsymbs,0);
 vector<unsigned> hist(numsymbs,0);
-//
-//
-//
 const int buffsize = 4096;
-const unsigned char_size = 8; // we translate, eg, "01001000" into 8+64 = 72 (the character).
-//
-//// this takes in a file and a buffer
-//// we read in a character from the infile, look up its encoding,
-//// and save that encoding into the buffer.
-//// (This assumes that the encoding is all character-based, etc. etc.
-////
-//// This returns the number of characters read from in.
-//// The number of bytes written to fake_bin_buff can be computed separately
-//// via the "room" method.
-//unsigned feed_huff_buff(istream& in, feed_buffer<char, buffsize>& huff_buff) {
-//    // we keep track of how much we've written.
-//    for (unsigned i = 0; ; ++i) {
-//        if (in.eof()) { return i; }
-//
-//        char to_encode = in.peek();
-//        string encoding = encodings[to_encode];
-//
-//        // if we have room for this encoding (may depend on value of to_encode)
-//        if (huff_buff.room() >= encoding.size()) {
-//            for (auto& c : encoding) {
-//                huff_buff.push(c);
-//            }
-//            in.get(to_encode); // pop! remove char from infile.
-//        }
-//        else {
-//            // if huff_buff is ``full'' (or close to it), we're done.
-//            return i;
-//        }
-//    }
-//}
-//
-//// surely there's some fancy bit twiddling we can do,
-//// but this is conceptually easier.
-//char trans_bin_string(const char a[char_size]) {
-//    char res = 0;
-//    for (unsigned i = 0; i < char_size; ++i) {
-//        if (a[i] == '1') {
-//            res += static_cast<int>(pow(2,i));
-//        }
-//    }
-//    return res;
-//}
-//
-//// given a ``fake binary buffer'' (really, an array of characters {'0', '1'}), convert those
-//// characters into real binary and save that encoding in real_bin_buff
-//// This is *very* machine specific, we assume bytes = 8 bits, endian stuff (??).
-//unsigned feed_bin_buff(feed_buffer<char, buffsize>& huff_buff, feed_buffer<char, buffsize>& bin_buff) {
-//    char to_trans[char_size];
-//    for (int i = 0; ; ++i) {
-//        
-//        // if we don't have enough bits to make a char, we're done
-//        if (huff_buff.remaining_data() < char_size) {
-//            return i;
-//        }
-//        // if our output buffer is full, we're done.
-//        if (bin_buff.room() == 0) {
-//            return i;
-//        }
-//        for (unsigned j = 0; j < char_size; ++j) {
-//            to_trans[j] = huff_buff.pop();
-//        }
-//        char bin_byte = trans_bin_string(to_trans);
-//        bin_buff.push(bin_byte);
-//    }
-//}
-//
-//// this simply pads the remaining data in huff_buff with 0s,
-//// brining the bit count up to 8.
-//// Padding with zeros is safe because of the prefix-free nature
-//// ---if we simply know to stop parsing the compressed file after
-//// x characters, we're good.
-//void resolve_last_byte(feed_buffer<char, buffsize>& huff_buff,
-//                       feed_buffer<char, buffsize>& bin_buff) {
-//    if (huff_buff.remaining_data() == 0) { return; }
-//    assert(huff_buff.remaining_data() < char_size);
-//    assert(bin_buff.room() > 0);
-//
-//    // just pad with zeros and dump it to bin_buff
-//    while (huff_buff.room() < char_size) {
-//        huff_buff.push('0');
-//    }
-//    huff_to_bin(huff_buff, bin_buff);
-//    assert(huff_buff.remaining_data() == 0);
-//}
-//
-//// the hard part is dealing with the edge cases---essentially, what if 
-//void write_compressed_file(istream& infile, ostream& outfile) {
-//    // we declare some necessary buffers.
-//    feed_buffer<char, buffsize> huff_buff, bin_buff;
-//    
-//
-//    while (!infile.eof()) {
-//        // read as many bytes as you can into fake_bin_buff
-//        auto bytes_read = feed_huff_buff(infile, huff_buff);
-//        // after this command, presumably huff_buff is ``full'' (or close to it).
-//        
-//        // take 8-byte-chunks from huff, and turn them into chars in bin_buff
-//        auto bytes_tran = huff_to_bin(huff_buff, bin_buff);
-//        // having emptied (or almost emptied) the huff_buff, we realign.
-//        huff_buff.realign();
-//
-//        // the first number should be much larger than the second
-//        cout << bytes_read << " " << bytes_tran << endl;
-//
-//        // only write to the file if the binary buffer is full
-//        if (bin_buff.room() == 0) {
-//            outfile.write(bin_buff.data(), bin_buff.remaining_data());
-//            // because we use file writing with a buffer,
-//            // we must realign ASAP!
-//            bin_buff.clear();
-//        }
-//        // NB: there may be a really weird boundary condition
-//        // when there are encodings close-to-buffsize, in
-//        // which we may never progress. Characterizing this would be
-//        // a useful, albeit boring, step.
-//        // Presumably this will be rare, however.
-//    }
-//
-//    // at this point we have consumed the infile.
-//    // there may be a lot of data left over in huff_buff and bin_buff,
-//    // so we preliminarily empty them.
-//    huff_to_bin(huff_buff, bin_buff);
-//    outfile.write(bin_buff.data(), bin_buff.remaining_data());
-//    bin_buff.clear();
-//
-//    // at this point bin_buff is empty, and presumably can't be filled
-//    // by whatever remains of huff_buff.
-//    huff_to_bin(huff_buff, bin_buff);
-//    assert(huff_buff.remaining_data() < 8);
-//    assert(bin_buff.room() > 0);
-//
-//    // we need to "cap" the bin_buff before the final write to the file,
-//    // so we've encoded the last bits.
-//    resolve_last_byte(huff_buff, bin_buff);
-//
-//    outfile.write(bin_buff.data(), bin_buff.remaining_data());
-//    // realign is needless---we should be all set.
-//}
-//
-//
+const unsigned byte_size = 8; // we translate, eg, "01001000" into 8+64 = 72 (the character).
+
+typedef unsigned char byte;
+
+class bit_to_byte_masks : public array<byte, 8> {
+    public:
+    bit_to_byte_masks() {
+        for (auto i = 0; i < 8; ++i) {
+            (*this)[i] = 1 << i;
+        }
+    }
+};
+
+bit_to_byte_masks masks;
+
+byte bits_to_byte(const array<bool,buffsize>& b, const unsigned n = 8) {
+    // i hope this is an actual all-zero pattern.
+    byte d = 0;
+    for (int i = 0; i < n; ++i) {
+        if (b[i]) {
+            d |= masks[i];
+        }
+    }
+    return d;
+}
+
+class encode_buffer {
+    public:
+        // default constructor...?
+        array<bool,buffsize> m;
+        unsigned next_empty = 0;
+    public:
+        bool put_byte(const byte b) {
+            if (encodings[b].size() < (buffsize - next_empty)) {
+                for (auto& c : encodings[b]) {
+                    m[next_empty++] = c == '1';
+                }
+                return true;
+            }
+            return false;
+        }
+        bool pop_byte(byte& b) {
+            // eg, if next_empty is 8, then [0..7] is defined, and that's a bytee
+            if (byte_size <= next_empty) {
+                b = bits_to_byte(m);
+                // having now popped the first 8 bits into b, we must
+                // slide our memory back (not efficient! i know...)
+                copy(m.begin()+byte_size, m.begin()+next_empty, m.begin());
+                next_empty -= 8;
+                return true;
+            }
+            return false;
+        }
+
+        // only use when contents < 8!
+        bool terminate(byte& b) {
+            // we have 7 or fewer bits...
+            if (next_empty < byte_size) {
+                b = bits_to_byte(m, next_empty);
+                next_empty = 0;
+                return true;
+            }
+            return false;
+        }
+};
+
+void encode(istream& infile, ostream& outfile) {
+    encode_buffer huffbuff;
+
+    while(!infile.eof()) {
+        bool buffer_has_room = true;
+        while(buffer_has_room) {
+            byte c = infile.peek();
+            
+            buffer_has_room = huffbuff.put_byte(c);
+
+            // if we successfully copied, pop
+            if (buffer_has_room) {
+                c = infile.get();
+            }
+        }
+
+        bool buffer_has_results = true;
+        while(buffer_has_results) {
+            byte c;
+
+            buffer_has_results = huffbuff.pop_byte(c);
+            
+            if (buffer_has_results) {
+                outfile.put(c);
+            }
+        }
+    }
+
+    assert(infile.eof());
+    assert(huffbuff.next_empty < byte_size);
+
+    byte c;
+
+    bool buffer_terminate = huffbuff.terminate(c);
+    assert(buffer_terminate);
+    outfile.put(c);
+}
+
+class decode_buffer {
+    public:
+        // default constructor...?
+        array<bool,buffsize> m;
+        unsigned next_empty = 0;
+    public:
+        bool put_byte(const byte b) {
+            // if buffsize is 256 (say), then
+            // the last index is 255, and say we have next_empty = 248,
+            // then 248, 249, 250, 251, 252, 253, 254, 255 are empty.
+            // and 256-248 = 8
+            if (buffsize - next_empty >= byte_size) {
+                for (auto i = 0; i < byte_size; ++i) {
+                    // i think this works...?
+                    m[next_empty++] = (b & masks[i]) == masks[i];
+                }
+                return true;
+            }
+            return false;
+        }
+        bool pop_byte(byte& b) {
+            auto t = hufftree;
+            unsigned bit_loc = 0;
+
+            // while we may still have a full tree-path in the buffer...
+            while(bit_loc < next_empty) {
+                if (t->is_leaf()) {
+                    b = t->i;
+                    copy(m.begin()+bit_loc, m.begin()+next_empty, m.begin());
+                    next_empty -= bit_loc;
+                    return true;
+                }
+                if (m[bit_loc++]) {
+                    t = t->r;
+                }
+                else {
+                    t = t->l;
+                }
+            }
+            return false;
+        }
+};
+
+void decode(istream& infile, ostream& outfile) {
+    const int bytes_to_write = 16;
+    int written = 0;
+    decode_buffer huffbuff;
+
+    while(!infile.eof()) {
+        bool buffer_has_room = true;
+        while(buffer_has_room) {
+            byte c = infile.peek();
+            
+            buffer_has_room = huffbuff.put_byte(c);
+
+            // if we successfully copied, pop
+            if (buffer_has_room) {
+                c = infile.get();
+            }
+        }
+
+        bool buffer_has_results = true;
+        while(buffer_has_results) {
+            byte c;
+
+            buffer_has_results = huffbuff.pop_byte(c);
+            
+            if (buffer_has_results) {
+                outfile.put(c);
+                written++;
+            }
+            if (written == bytes_to_write) {
+                return;
+            }
+        }
+    }
+}
 //
 // read through the file.
 // returns the number of bytes read.
 unsigned compute_freqs(istream& in) {
     unsigned total = 0;
     while (!in.eof()) {
-        unsigned char a = in.get();
+        byte a = in.get();
         hist[a] += 1;
         total += 1;
     }
@@ -175,39 +215,19 @@ unsigned compute_freqs(istream& in) {
     return total;
 }
 
-class decoder_buffer {
-    private:
-    bitset<buffsize> m;
-    unsigned next_empty = 0;
-    unsigned datasize() {
-        return next_empty;
-    }
-    unsigned room() {
-        assert(buffsize > nextempty);
-        // if we have written 100 bytes, then
-        // nextempty is at slot 99.
-        return buffsize - (nextempty+1);
-    }
-    public:
-    bool canstore(char c) const {
-        auto size = encodings[c].size();
-        return size <= room();
-    }
-    char popchar() {
-        if (datasize() >= 8) {
-            char c = 0;
-
-        }
-    }
-    char terminate() {
-        return 'c';
-    }
-    char pushsymb(char c) {
-        return 'c';
-    }
-};
 
 
+// this "works", from my ad-hoc testing. That outputs 17, for instansce.
+//    array<bool,buffsize> a;
+//    a[0] = 1;
+//    a[1] = 0;
+//    a[2] = 0;
+//    a[3] = 0;
+//    a[4] = 1;
+//    a[5] = 0;
+//    a[6] = 0;
+//    a[7] = 0;
+//    cout << (int)bits_to_byte(a) << endl;
 
 const string usage_string = "Usage: ./huffman -e < file > encoding_description\n"
                             "       ./huffman -c encoding_description < orig_file > compression\n"
@@ -225,8 +245,15 @@ int main(int argc, char* argv[]) {
     else if (argc == 3 && strncmp(argv[1], "-c",2) == 0) {
         cerr << "Compressing!" << endl;
         ifstream tree_file(argv[2]);
-        auto tree = read_htree(tree_file);
-        generate_encodings(tree, encodings);
+        hufftree = read_htree(tree_file);
+        generate_encodings(hufftree, encodings);
+        encode(cin, cout);
+    }
+    else if (argc == 3 && strncmp(argv[1], "-d",2) == 0) {
+        cerr << "Decompressing!" << endl;
+        ifstream tree_file(argv[2]);
+        hufftree = read_htree(tree_file);
+        decode(cin, cout);
     }
     else {
         cout << usage_string;
